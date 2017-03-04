02616433cb710fd63173d7e81bb3fd227319274d
killeen <killeen@uchicago.edu>
2017-03-04 11:32:35 -0600

    changed the test cube

--- test.hs
+++ test.hs
@@ -2,26 +2,26 @@ module Test (test) where
 
 import Cube
 
-test = Cube [Corner (Tile F U) (Tile U B) (Tile L L),
-             Edge (Tile U B) (Tile F R),
-             Corner (Tile F R) (Tile U U) (Tile R B),
-             Edge (Tile R F) (Tile F U),
-             Corner (Tile F D) (Tile R B) (Tile D L),
-             Edge (Tile D B) (Tile F D),
-             Corner (Tile F F) (Tile D D) (Tile L L),
-             Edge (Tile L D) (Tile F R),
+test = Cube [Corner (Tile F L) (Tile U B) (Tile L D),
+             Edge   (Tile U F) (Tile F U),
+             Corner (Tile F B) (Tile U D) (Tile R R),
+             Edge   (Tile R D) (Tile F B),
+             Corner (Tile F R) (Tile R D) (Tile D F),
+             Edge   (Tile D U) (Tile F B),
+             Corner (Tile F F) (Tile D U) (Tile L R),
+             Edge   (Tile L R) (Tile F U),
 
-             Edge (Tile U U) (Tile L L),
-             Edge (Tile U D) (Tile R L),
-             Edge (Tile D U) (Tile R R),
-             Edge (Tile L B) (Tile D L),
+             Edge   (Tile U R) (Tile L D),
+             Edge   (Tile U D) (Tile R F),
+             Edge   (Tile D B) (Tile R L),
+             Edge   (Tile L R) (Tile D B),
 
-             Corner (Tile U F) (Tile L R) (Tile B D),
-             Edge (Tile B B) (Tile U U),
-             Corner (Tile U F) (Tile R R) (Tile B U),
-             Edge (Tile R F) (Tile B R),
-             Corner (Tile B U) (Tile R L) (Tile D F),
-             Edge (Tile B D) (Tile D F),
-             Corner (Tile B B) (Tile L D) (Tile D R),
-             Edge (Tile B L) (Tile L F)
+             Corner (Tile U L) (Tile L B) (Tile B U),
+             Edge   (Tile B L) (Tile U D),
+             Corner (Tile U R) (Tile R B) (Tile B U),
+             Edge   (Tile R F) (Tile B L),
+             Corner (Tile B F) (Tile R U) (Tile D L),
+             Edge   (Tile B R) (Tile D F),
+             Corner (Tile B D) (Tile L L) (Tile D F),
+             Edge   (Tile B U) (Tile L L)
              ]
